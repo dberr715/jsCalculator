11ee1a1ad61ca796beb00a2dcdf0c49086dc0702
David Berry <dberry3755@gmail.com>
2023-09-11 17:01:48 -0400

    initial commit with css for calculator

---
+++ main.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function (){
+    console.log ("Everything is loaded!!!");
+
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const seven = document.getElementById('seven');
+    const eight = document.getElementById('eight');
+    const nine = document.getElementById('nine');
+    const zero = document.getElementById('zero');
+    const plus = document.getElementById('plus');
+    const minus = document.getElementById('minus');
+    const times = document.getElementById('times');
+    const divide = document.getElementById('divide');
+    const dot = document.getElementById('dot');
+    const clear = document.getElementById('clear');
+    const displayArea = document.getElementById('displayArea');
+
+   
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
